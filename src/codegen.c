41b2ac402ae5c6ef1d1b6d16a79a4c03bdaa4894
garfr <gavratcliff@gmail.com>
2021-03-20 21:00:01 -0500

    Fixed my horrible sin

--- src/codegen.c
+++ src/codegen.c
@@ -100,78 +100,18 @@ int translateCharacter(Symbol sym) {
 
     // clang-format off
     if (sym.len >= 1){
-        switch (sym.text[0]) {
-            case 'A': return 65;
-            case 'B': return 66;
-            case 'C': return 67;
-            case 'D': return 68;
-            case 'E': return 69;
-            case 'F': return 70;
-            case 'G': return 71;
-            case 'H': return 72;
-            case 'I': return 73;
-            case 'J': return 74;
-            case 'K': return 75;
-            case 'L': return 76;
-            case 'M': return 77;
-            case 'N': return 78;
-            case 'O': return 79;
-            case 'P': return 80;
-            case 'Q': return 81;
-            case 'R': return 82;
-            case 'S': return 83;
-            case 'T': return 84;
-            case 'U': return 85;
-            case 'V': return 86;
-            case 'W': return 87;
-            case 'X': return 88;
-            case 'Y': return 89;
-            case 'Z': return 90;
-            case 'a': return 97;
-            case 'b': return 98;
-            case 'c': return 99;
-            case 'd': return 100;
-            case 'e': return 101;
-            case 'f': return 102;
-            case 'g': return 103;
-            case 'h': return 104;
-            case 'i': return 105;
-            case 'j': return 106;
-            case 'k': return 107;
-            case 'l': return 108;
-            case 'm': return 109;
-            case 'n': return 110;
-            case 'o': return 111;
-            case 'p': return 112;
-            case 'q': return 113;
-            case 'r': return 114;
-            case 's': return 115;
-            case 't': return 116;
-            case 'u': return 117;
-            case 'v': return 118;
-            case 'w': return 119;
-            case 'x': return 120;
-            case 'y': return 121;
-            case 'z': return 122;
-// clang-format on 
-            case '\\':
-                      if (sym.len < 2) {
+        if (sym.text[0] == '\\') {
 
-                          printf("invalid char\n");
-                          exit(1);
-                      }
-                      switch (sym.text[1]) {
+            if (sym.text[1] == 'n') {
 
-                          case 'n':
-                              return 10;
-                            default:
-                              exit(1);
-                      }
-
-
+                return 10;
+            }
         }
-    } 
-    printf("Invalid char\n");
+        else {
+            return (int) sym.text[0];
+        }
+    }
+    printf("Symbol too short.\n");
     exit(1);
 }
 
